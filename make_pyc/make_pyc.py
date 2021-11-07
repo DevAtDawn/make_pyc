23e658f1d7030d1e2b6ec7619a32e65bdcf9c968
DevAtDawn <86164137+DevAtDawn@users.noreply.github.com>
2021-11-07 13:17:55 -0700

    Update make_pyc.py

--- make_pyc/make_pyc.py
+++ make_pyc/make_pyc.py
@@ -2,13 +2,14 @@ import py_compile
 import sys
 import pathlib
 from pathlib import Path
+cwd = Path.cwd()
 
 def main():
     files = sys.argv
     for x in files[1:]:
         p = Path(x)
         out_file = str(p.with_name(p.stem).with_suffix('.pyc'))
-        py_compile.compile(x, out_file)
+        py_compile.compile(x, cwd / out_file)
         
 if __name__ == "__main__":
     main()
